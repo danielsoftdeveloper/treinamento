1a42ea5b72f08aa7280f534c9592d7e321e2a160
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-05-05 11:21:50 -0300

    Criado botoes de controles de audio e habilitado controle na tela.

--- recriando-netflix/js/videojs/video.js
+++ recriando-netflix/js/videojs/video.js
@@ -8,21 +8,21 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // Quando clica no botão mostra o modal com vídeo.
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
-}
+};
 
 //uando usuário clica em <span> (x), fecha o modal de filmes
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
 // Quando o usuário clica fora fecha o modal de filmes
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
 /*  */
 // Recebe o modal de informaçõe
@@ -33,23 +33,23 @@ var btn2 = document.getElementById("myBtn2");
 
 // Recebe o <span> elemento ue fecha o modal
 var span2 = document.getElementsByClassName("close")[1];
- 
+
 // Quando clica no botão mostra o modal de informações.
-btn2.onclick = function() {
+btn2.onclick = function () {
   modal2.style.display = "block";
-}
+};
 
 //uando usuário clica em <span> (x), fecha o modal de filmes
-span2.onclick = function() {
+span2.onclick = function () {
   modal2.style.display = "none";
-}
+};
 
 // Quando o usuário clica fora fecha o modal de filmes
-window.onclick = function(event2) {
+window.onclick = function (event2) {
   if (event2.target == modal2) {
     modal2.style.display = "none";
   }
-} 
+};
 
 /* script para pausar e da play no vídeo */
 
@@ -64,6 +64,21 @@ function myFunction() {
   } else {
     video.pause();
     btn.innerHTML = "Play";
-    
   }
+}
+
+/* habilita audio */
+
+var vid = document.getElementById("myVideo");
+
+function enableMute() {
+  vid.muted = true;
+}
+
+function disableMute() {
+  vid.muted = false;
+}
+
+function checkMute() {
+  alert(vid.muted);
 }
