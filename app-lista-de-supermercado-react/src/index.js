520ddac2bea8fca61715f2db3c33d14dca3125cf
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-06-02 16:04:56 -0300

    Iniciado a criaçao dos apps de supermercado React e React Native

--- app-lista-de-supermercado-react/src/index.js
+++ app-lista-de-supermercado-react/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './Action/componentes/ClassName.css';
+import App from './Action/Action.jsx';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
