0bd968b2538ab8026881069a21b93da1eeb62267
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-06-03 19:59:37 -0300

    Criando segundo componente passando paramentro.

--- fundamentos-react/src/components/Primeiro.jsx
+++ fundamentos-react/src/components/Primeiro.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default () => (
   <React.Fragment>
-    <h1>Primeiro Componente</h1>
+    <h1>Primeiro Componente 1</h1>
     <h2>Exemplo de um componente</h2>
   </React.Fragment>
 );
--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,10 +1,17 @@
-import './index.css'
-import React from 'react'
-import ReactDOM from 'react-dom'
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Primeiro from './components/Primeiro'
+import Primeiro from "./components/Primeiro";
+import ComParametro from "./components/ComParametro";
 
 ReactDOM.render(
-    <Primeiro></Primeiro>,
-    document.getElementById('root')
-)
+  <div>
+    <Primeiro />
+    <ComParametro titulo='Este é o titulo' 
+    titulo='Esse é o titulo' />
+    <ComParametro titulo='Este é o subtitulo' 
+    paragrafo='Esse e o paragrafo' />
+  </div>,
+  document.getElementById("root")
+);
