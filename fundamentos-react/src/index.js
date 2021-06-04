e0c0dcdea6307144797a92f4421b54b5611b6184
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-06-04 11:00:56 -0300

    Passando parametro para para os filhos do componente

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,16 +2,28 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Primeiro from "./components/Primeiro";
-import ComParametro from "./components/ComParametro";
+//import Primeiro from "./components/Primeiro";
+//import ComParametro from "./components/ComParametro";
+import ComFilhos from './components/ComFilhos'
 
 ReactDOM.render(
   <div>
-    <Primeiro />
+
+  <ComFilhos>
+    <ul>
+      <li>Ana</li>
+      <li>Bia</li>
+      <li>Carlos</li>
+      <li>Daniel</li>
+    </ul>
+  </ComFilhos>
+
+   {/*  <Primeiro />
     <ComParametro titulo='Este é o titulo'
      subtitulo='Esse é o subtitulo' />
      <ComParametro titulo='Epa'
      subtitulo='Opa' />  
+ */}
   </div>,
   document.getElementById("root")
 );
