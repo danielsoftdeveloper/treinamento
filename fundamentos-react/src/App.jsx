15fb61161580978ed3dd717aea724354c83f5a29
danielsoftdeveloper <daniel.andrade@sga.pucminas.br>
2021-06-09 11:55:22 -0300

    Criado Condicional versao 2 com if utilizando funçao

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -7,10 +7,14 @@ import Card from "./components/layout/Card";
 import Primeiro from "./components/Primeiro";
 import Repeticao from "./components/Repeticao";
 import Condicional from "./components/Condicional";
+import CondicionalComIf from "./components/CondicionalComIf";
 
 export default (props) => (
-
   <div className='App'>
+    <Card titulo='06 - Condicional v2 '>
+      <CondicionalComIf numero={10}></CondicionalComIf>
+    </Card>
+
     <Card titulo='05 - Condicional v1 '>
       <Condicional numero={11}></Condicional>
     </Card>
